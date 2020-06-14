2ed7d64bff6172bdd7796026395d0d41c2e9e070
Juan Chavez <juanechavez16@gmail.com>
2020-06-14 17:44:29 -0400

    2 configs - not working

--- source/queue-manager.h
+++ source/queue-manager.h
@@ -240,32 +240,45 @@ void SimplePDWorld::PrintNeighborInfo(std::ostream& os) {
     }
     os.flush();
 }
-namespace emp {
+
+EMP_BUILD_CONFIG(Config,
+                 GROUP(DEFAULT_GROUP, "General Settings"),
+                 VALUE(ID, size_t, 0, "table cell id"),
+                 VALUE(R, double, 0.0, "neighborhood radius"),
+                 VALUE(U, double, 0.0, "cost/benefit ratio"),
+                 VALUE(N, size_t, 0, "population size"),
+                 VALUE(E, size_t, 0, "amount of epochs a population should run for"), )
+
+EMP_BUILD_CONFIG(MyConfig,
+                 GROUP(DEFAULT_GROUP, "General Settings"),
+                 VALUE(ID, size_t, 0, "table cell id"),
+                 VALUE(R, double, 0.0, "neighborhood radius"),
+                 VALUE(U, double, 0.0, "cost/benefit ratio"),
+                 VALUE(N, size_t, 0, "population size"),
+                 VALUE(E, size_t, 0, "amount of epochs a population should run for"), )
+
 struct RunInfo {
-    /*
     size_t id;
     double r;
     double u;
     size_t N;
     size_t E;
-    */
 
-    Config& config;
+    //emp::Config& config;
 
     size_t cur_epoch;
     size_t num_coop;
     size_t num_defect;
 
-    /*
     RunInfo(size_t _id, double _r, double _u, size_t _N, size_t _E)
         : id(_id), r(_r), u(_u), N(_N), E(_E), cur_epoch(0), num_coop(0), num_defect(0) { ; }
-    */
 
-    RunInfo(Config& config_) : config(config_), cur_epoch(0), num_coop(0), num_defect(0) { ; }
+    //RunInfo(emp::Config& config_) : config(config_), cur_epoch(0), num_coop(0), num_defect(0) { ; }
 };
 
 class QueueManager {
    private:
+    //MyConfig& my_config;
     std::queue<RunInfo> runs;
     emp::web::Div my_div_;
     std::string table_id;
@@ -285,8 +298,8 @@ class QueueManager {
     }
 
     /// Adds run to queue with run info for paramters
-    void AddRun(Config& config) {
-        RunInfo new_run(config);
+    void AddRun(size_t id, double r, double u, size_t N, size_t E) {
+        RunInfo new_run(id, r, u, N, E);
         runs.push(new_run);
     }
 
@@ -303,7 +316,7 @@ class QueueManager {
     }
 
     /* Possibly make dive a class of its own */
-    /// Returns this dic
+    /// Returns this div
     emp::web::Div GetDiv() {
         return my_div_;
     }
@@ -399,4 +412,3 @@ class QueueManager {
         my_div_ << my_button;
     }
 };
-}  // namespace emp
